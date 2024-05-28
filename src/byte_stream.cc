98732912f5a80c1314f8815f5ee79d0d9072196d
kazusa <i@kazusa.me>
2024-05-28 10:31:49 +0800

    (lab00) feat: byte stream

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -1,4 +1,5 @@
 #include "byte_stream.hh"
+#include <algorithm>
 
 using namespace std;
 
@@ -6,60 +7,56 @@ ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
 
 bool Writer::is_closed() const
 {
-  // Your code here.
-  return {};
+  return is_closed_;
 }
 
 void Writer::push( string data )
 {
-  // Your code here.
-  (void)data;
-  return;
+  uint64_t sz = min( available_capacity(), data.size() );
+  std::copy_n( data.begin(), sz, back_inserter( buffer_ ) );
+  bytes_pushed_ += sz;
 }
 
 void Writer::close()
 {
-  // Your code here.
+  is_closed_ = true;
 }
 
 uint64_t Writer::available_capacity() const
 {
-  // Your code here.
-  return {};
+  return capacity_ - buffer_.size();
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  // Your code here.
-  return {};
+  return bytes_pushed_;
 }
 
 bool Reader::is_finished() const
 {
-  // Your code here.
-  return {};
+  return is_closed_ && buffer_.size() == 0;
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  // Your code here.
-  return {};
+  return bytes_poped_;
 }
 
 string_view Reader::peek() const
 {
-  // Your code here.
-  return {};
+  if ( buffer_.empty() )
+    return {};
+  return string_view { reinterpret_cast<const char*>( buffer_.data() ), buffer_.size() };
 }
 
 void Reader::pop( uint64_t len )
 {
-  // Your code here.
-  (void)len;
+  uint64_t sz = min( len, buffer_.size() );
+  buffer_.erase( buffer_.begin(), buffer_.begin() + sz );
+  bytes_poped_ += sz;
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  // Your code here.
-  return {};
+  return buffer_.size();
 }
--- src/byte_stream.hh
+++ src/byte_stream.hh
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <string>
 #include <string_view>
+#include <vector>
 
 class Reader;
 class Writer;
@@ -25,6 +26,10 @@ protected:
   // Please add any additional state to the ByteStream here, and not to the Writer and Reader interfaces.
   uint64_t capacity_;
   bool error_ {};
+  std::vector<uint8_t> buffer_;
+  bool is_closed_ {};
+  uint64_t bytes_pushed_ {};
+  uint64_t bytes_poped_ {};
 };
 
 class Writer : public ByteStream
